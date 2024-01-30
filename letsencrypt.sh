410f8c48ef908276741ca518074da3843e3e49be
dinesh883248 <112616399+dinesh883248@users.noreply.github.com>
2024-01-30 17:29:43 +0530

    letsencrypt certs using lego

---
+++ letsencrypt.sh
@@ -0,0 +1,5 @@
+# use lego for ssl
+```sh
+GOOGLE_DOMAINS_ACCESS_TOKEN=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+lego --email you@example.com --dns googledomains --domains 'first.example.org' --domains 'second.example.org' run
+```
