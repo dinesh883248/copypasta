cb393c79ffa805d2519acd7e904725cb3dd458f1
dinesh883248 <112616399+dinesh883248@users.noreply.github.com>
2025-10-14 18:30:30 +0530

    Create setup-notebook.sh

---
+++ remote-notebook/setup-notebook.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USER=$1
+REMOTE_IP=$2
+PRIVATE_KEY_PATH=$3
+
+if [ -z "$USER" ]; then
+  echo "Usage: $0 <user> <remote_ip> [private_key_path]"
+  exit 1
+fi
+
+if [ -z "$REMOTE_IP" ]; then
+  echo "Usage: $0 <user> <remote_ip> [private_key_path]"
+  exit 1
+fi
+
+if [ -z "$PRIVATE_KEY_PATH" ]; then
+  PRIVATE_KEY_PATH="~/.ssh/id_rsa"
+fi
+
+rsync -azP -e "ssh -i $PRIVATE_KEY_PATH" run-notebook.sh $USER@$REMOTE_IP:/home/$USER/run-notebook.sh
+
+ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -i $PRIVATE_KEY_PATH $USER@$REMOTE_IP "chmod +x /home/$USER/run-notebook.sh"
+
+ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -i $PRIVATE_KEY_PATH $USER@$REMOTE_IP "/home/$USER/run-notebook.sh $USER"
+
+echo "Run the following command in your local terminal to access the Jupyter Notebook:"
+echo "ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -i $PRIVATE_KEY_PATH -L 8899:localhost:8899 $USER@$REMOTE_IP"
