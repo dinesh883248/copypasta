8a514814e9ec1001bcc7cd9a9c2dfa2e4cdfdf51
dinesh883248 <112616399+dinesh883248@users.noreply.github.com>
2025-10-14 18:33:53 +0530

    Rename run-notebook.sh to install-notebook.sh

--- remote-notebook/setup-notebook.sh
+++ remote-notebook/setup-notebook.sh
@@ -18,11 +18,11 @@ if [ -z "$PRIVATE_KEY_PATH" ]; then
   PRIVATE_KEY_PATH="~/.ssh/id_rsa"
 fi
 
-rsync -azP -e "ssh -i $PRIVATE_KEY_PATH" run-notebook.sh $USER@$REMOTE_IP:/home/$USER/run-notebook.sh
+rsync -azP -e "ssh -i $PRIVATE_KEY_PATH" run-notebook.sh $USER@$REMOTE_IP:/home/$USER/install-notebook.sh
 
-ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -i $PRIVATE_KEY_PATH $USER@$REMOTE_IP "chmod +x /home/$USER/run-notebook.sh"
+ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -i $PRIVATE_KEY_PATH $USER@$REMOTE_IP "chmod +x /home/$USER/install-notebook.sh"
 
-ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -i $PRIVATE_KEY_PATH $USER@$REMOTE_IP "/home/$USER/run-notebook.sh $USER"
+ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -i $PRIVATE_KEY_PATH $USER@$REMOTE_IP "/home/$USER/install-notebook.sh $USER"
 
 echo "Run the following command in your local terminal to access the Jupyter Notebook:"
 echo "ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -i $PRIVATE_KEY_PATH -L 8899:localhost:8899 $USER@$REMOTE_IP"
